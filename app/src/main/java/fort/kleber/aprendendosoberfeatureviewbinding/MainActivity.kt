24000ef29d5c2f0b6b8e1443ae6a0865d7dacb2e
kleberfort <kleberfort@gmail.com>
2022-03-15 14:01:20 -0300

    Inserido a feature de viewBinding e implementado na classe Main kotlin

--- app/src/main/java/fort/kleber/aprendendosoberfeatureviewbinding/MainActivity.kt
+++ app/src/main/java/fort/kleber/aprendendosoberfeatureviewbinding/MainActivity.kt
@@ -2,10 +2,24 @@ package fort.kleber.aprendendosoberfeatureviewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import fort.kleber.aprendendosoberfeatureviewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+    //como não vai ser inicializado no construtor, coloco lateinit
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        //val tvHello = findViewById<TextView>(R.id.tvhello)
+
+         binding = ActivityMainBinding.inflate(layoutInflater)
+         setContentView(binding.root)
+         binding.tvhello.text = "teste com viewBinding"
+
     }
 }
